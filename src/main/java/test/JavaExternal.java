6c8d7faf59344926c7d0fa22ebf1fb86b1c98779
timeraider@gmx.at <timeraider@gmx.at>
2016-11-20 02:00:26 +0100

    initial commit

---
+++ src/main/java/test/JavaExternal.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.apache.log4j.Logger;
+
+public class JavaExternal {
+	
+	public Logger testExt() {
+		return Logger.getLogger("JavaExternal");
+	}
+	
+}
---
+++ src/main/java/test/XtendExternal.xtend
@@ -0,0 +1,11 @@
+package test
+
+import org.apache.log4j.Logger
+
+class XtendExternal {
+	
+	def Logger testExt() {
+		return Logger.getLogger("XtendExternal");
+	}
+	
+}
